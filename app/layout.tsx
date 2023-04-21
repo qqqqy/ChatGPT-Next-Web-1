b8e5c59948941ca92bd1784d9931a601a8fd3499
随遇而安 <17608718897@qq.com>
2023-04-21 14:38:00 +0800

    增加登录控制

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "学习助手",
+  title: "学习助手 ",
   description: "",
   appleWebApp: {
-    title: "学习助手",
+    title: "学习助手 ",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
